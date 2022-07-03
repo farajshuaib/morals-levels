260bb6e057a0cd29f876acad8d61188d835875b4
faraj Shuaib <46637985+farajshuaib@users.noreply.github.com>
2022-07-03 18:25:49 +0200

    register server worker

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,13 @@ import { ToastContainer, toast } from 'react-toastify';
 import "./firebase/config";
 import 'react-toastify/dist/ReactToastify.css';
 import "./styles/index.css";
+import { registerSW } from "virtual:pwa-register";
+
+if ("serviceWorker" in navigator) {
+  // && !/localhost/.test(window.location)) {
+  registerSW();
+}
+
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
